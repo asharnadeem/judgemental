bb187adda265fba38e8df5a00101ee8836d680cd
Ashar Nadeem <ashar@shipveho.com>
2023-02-17 12:32:09 -0500

    add more common methods

--- src/string.ts
+++ src/string.ts
@@ -5,107 +5,95 @@ const datetimeSchema = z.string().datetime({ offset: true });
 const emailSchema = z.string().email();
 const urlSchema = z.string().url();
 const uuidSchema = z.string().uuid();
+const whitespaceChars = [
+  "\u0009", // horizontal tabulation
+  "\u0020", // space
+  "\u000A", // line feed
+  "\u000B", // vertical tabulation
+  "\u000C", // form feed
+  "\u000D", // carriage return
+  "\u241C", // file separator
+  "\u241D", // group separator
+  "\u241E", // record separator,
+  "\u241F", // unit separator
+];
 
-/**
- * checks if string is a valid cuid
- *
- * @example
- * // returns true
- * "cjld2cjxh0000qzrmn831i7rn"
- * "cle6c54co000408mj0pfaa54o"
- *
- * @example
- * // returns false
- * "foo"
- * ""
- *
- * @param str - the input string
- * @returns returns a boolean denoting whether or not `str` is a valid cuid
- */
+export function containsWhitespace(str: string): boolean {
+  for (const s of str) {
+    if (isWhitespace(s)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export function equalsIgnoreCase(str1: string, str2: string) {
+  if (str1.toLowerCase() === str2.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export function isAlpha(str: string): boolean {
+  return /^[a-zA-Z]+$/.test(str);
+}
+
+export function isAlphaNumeric(str: string): boolean {
+  for (const s of str) {
+    if (!isAlpha(s) && !isNumeric) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function isBlank(str: string): boolean {
+  for (const s of str) {
+    if (!isWhitespace(s)) {
+      return false;
+    }
+  }
+  return true;
+}
+
 export function isCuid(str: string): boolean {
   return cuidSchema.safeParse(str).success;
 }
 
-/**
- * checks if string is a valid datetime
- *
- * @example
- * // returns true
- *
- * @example
- * // returns false
-
- *
- * @param str - the input string
- * @returns returns a boolean denoting whether or not `str` is a valid datetime
- */
 export function isDatetime(str: string): boolean {
   return datetimeSchema.safeParse(str).success;
 }
 
-/**
- * checks if string is a valid email
- *
- * @example
- * // returns true
- * "foo@bar.com"
- * "foo.bar@baz.qux.com"
- *
- * @example
- * // returns false
- * "foo.com"
- * "foo.bar.com"
- * ""
- *
- * @param str - the input string
- * @returns returns a boolean denoting whether or not `str` is a valid email
- */
 export function isEmail(str: string): boolean {
   return emailSchema.safeParse(str).success;
 }
 
-/**
- * checks if string is a valid url
- *
- * @example
- * // returns true
- * "https://www.foo.com"
- * "http://www.foo.com"
- * "www.foo.com"
- * "foo.com"
- * "foo.bar.com"
- *
- * @example
- * // returns false
- * "foo"
- * "123"
- * ""
- *
- * @param str - the input string
- * @returns returns a boolean denoting whether or not `str` is a valid url
- */
+export function isJson(str: string): boolean {
+  try {
+    JSON.parse(str);
+    return true;
+  } catch (_) {
+    return false;
+  }
+}
+
+export function isNumeric(str: string): boolean {
+  return !isNaN(parseFloat(str)) && isFinite(Number(str));
+}
+
 export function isUrl(str: string): boolean {
   return urlSchema.safeParse(str).success;
 }
 
-/**
- * checks if string is a valid uuid
- *
- * @example
- * // returns true
- * "f5d46a31-5f8d-4f17-864f-70f796263a34"
- * "b8720428-ad8e-11ed-afa1-0242ac120002"
- *
- * @example
- * // returns false
- * "123-456"
- * "foo-bar"
- * "foo"
- * ""
- *
- * @param str - the input string
- * @returns returns a boolean denoting whether or not `str` is a valid uuid
- */
 export function isUuid(str: string): boolean {
   return uuidSchema.safeParse(str).success;
+}
+
+export function isWhitespace(str: string): boolean {
+  const char = str.charAt(0);
+  if (!whitespaceChars.includes(char)) {
+    return false;
+  }
+  return true;
 }
